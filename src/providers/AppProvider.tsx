fb28e2d63b6789f2ba0d0b882598f4f7795cb729
Aniekan Sunday Udo <udoaniekan1995@gmail.com>
2026-07-04 07:41:45 +0100

    chore: consolidate imports

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,6 +1,5 @@
 import { useEffect, type ReactNode } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { useQuery, QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
 
 import {
